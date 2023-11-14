e857f090ceb4717a167d8ed47eeb528cd84d3a62
omar <omartamer5535002@gmail.com>
2023-11-14 19:30:14 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < myArr.length; index++) {
-  console.log(myArr[index]);
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
+const lang = 'C is fun';
+
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(lang);
   }
 }
