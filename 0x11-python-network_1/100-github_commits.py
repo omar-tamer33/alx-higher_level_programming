ec1ecfc59df7c2b59b0c7e82754d7d210c52a167
omar <omartamer5535002@gmail.com>
2023-12-03 21:40:30 +0000

    0x11-python-network_1-adv.-2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
 """
+Python script that takes 2 arguments in order to solve the given challenge
+"""
+import requests
+from sys import argv
 
-
-if __name__ == "__main__":
-    from requests import get
-    from sys import argv
-
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    commits = get(url).json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
