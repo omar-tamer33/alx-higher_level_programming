257b029478d951462f8d8524d843dfe4c44c3160
omar <omartamer5535002@gmail.com>
2024-01-04 00:10:55 +0000

    0x14-javascript-web_scraping+

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
